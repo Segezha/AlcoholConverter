cd40f3db8abcda7e46cabe56d463085ab31602a9
Segezha <maxpetrov2001@yandex.ru>
2022-12-26 18:42:32 +0300

    Added ethanol amount evaluation

--- src/main/java/segezha/AlcoholConverter/Services/AlcoholConverterService.java
+++ src/main/java/segezha/AlcoholConverter/Services/AlcoholConverterService.java
@@ -9,4 +9,19 @@ public class AlcoholConverterService {
         return firstVolume * firstStrength / secondStrenght;
     }
 
+    public Double alcoholAmount(Double strength, Double volume, String gender, Double weight) {
+        double ethanol = strength * volume / 100;
+        if (gender.equals("male")) return ethanol / (weight * 0.7);
+        else if (gender.equals("female")) return ethanol / (weight * 0.6);
+        else return -1.0;
+    }
+
+    public Double alcoholAmountForTwo(Double firstStrength, Double firstVolume, Double secondStrenght,
+                                      Double secondVolume, String gender, Double weight) {
+        double ethanol = firstStrength * firstVolume / 100 + secondStrenght + secondVolume;
+        if (gender.equals("male")) return ethanol / (weight * 0.7);
+        else if (gender.equals("female")) return ethanol / (weight * 0.6);
+        else return -1.0;
+    }
+
 }
